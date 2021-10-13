d9725bd099419736ea96b85871b2b056e0c4cdf1
Dani <danielpereiragarcia97@gmail.com>
2021-10-13 20:07:01 +0200

    Le damos color aleatoria al boton. Ademas cada boton se crea en una fila diferente. Ycreamos un boton que borra todas las filas

--- app/src/main/java/com/example/vistasdinamicascontablelayout/MainActivity.java
+++ app/src/main/java/com/example/vistasdinamicascontablelayout/MainActivity.java
@@ -2,15 +2,19 @@ package com.example.vistasdinamicascontablelayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
 
     TableLayout tabla;
+    int contadorFila=0;
 //hay que crear un boton para añadir botones y otro para añadir filas. Ademas de que esos botones hagan algo
 
     @Override
@@ -28,10 +32,21 @@ public class MainActivity extends AppCompatActivity {
         crearBotonEnFila();
     }
     public void crearBotonEnFila(){
-        TableRow fila=(TableRow)tabla.getChildAt(0);
+        TableRow fila=(TableRow)tabla.getChildAt(contadorFila);
 
         Button btn = new Button(this);
-        btn.setText("Botón creado en la fila");
+        btn.setText("Hola");
+
+        Random r= new Random();
+        int color = Color.rgb(r.nextInt(256),r.nextInt(256),r.nextInt(256));
+        btn.setBackgroundColor(color);
+
         fila.addView(btn);
+        contadorFila++;
+    }
+
+    public void borrarFilas(View v){
+        tabla.removeAllViews();
+        contadorFila=0;
     }
 }
